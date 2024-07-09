2db94700b35b967dda9e5d276ba600297ee08b22
Paul Gathondu <aulgondu@gmail.com>
2024-07-08 19:16:11 -0700

    chore: fmt change

--- crates/nest/migrations/20240709013544_add-new-columns.up.sql
+++ crates/nest/migrations/20240709013544_add-new-columns.up.sql
@@ -2,3 +2,4 @@
 ALTER TABLE browser_tabs ADD COLUMN muted BOOLEAN;
 ALTER TABLE browser_tabs ADD COLUMN reason TEXT;
 ALTER TABLE browser_tabs ADD COLUMN extension_id TEXT;
+ALTER TABLE browser_tabs DROP COLUMN muted_info;
