611e97b8890f90738de6e01465e94e9bc68a1c6c
Paul Gathondu <aulgondu@gmail.com>
2024-07-27 14:01:50 -0700

    fix: make update_tabs no error

--- crates/nest/src/http/handlers/update_tabs.rs
+++ crates/nest/src/http/handlers/update_tabs.rs
@@ -1,12 +1,17 @@
-use anyhow::Ok;
 use axum::{extract::State, Json};
 use sqlx::SqlitePool;
 
 use crate::http::schema::browser_tab::BrowserTabs;
+use crate::http::error::Error;
 
-pub async fn update_tab(State(pool): State<SqlitePool>, Json(tab): Json<BrowserTabs>) -> Result<Json<BrowserTabs>, String> {
-	let result = sqlx::query!(
-        "UPDATE browser_tabs SET status = ?, tab_index = ?, opener_tab_id = ?, title = ?, url = ?, pending_url = ?, pinned = ?, highlighted = ?, window_id = ?, active = ?, favicon_url = ?, incognito = ?, selected = ?, audible = ?, discarded = ?, auto_discardable = ?, muted = ?, muted_reason = ?, muted_extension_id = ?, width = ?, height = ?, session_id = ?, group_id = ?, last_accessed = ? WHERE id = ?",
+pub async fn update_tab(State(pool): State<SqlitePool>, Json(tab): Json<BrowserTabs>) -> Result<Json<BrowserTabs>, Error> {
+	sqlx::query!(
+        r#" 
+        UPDATE browser_tabs SET status = ?, tab_index = ?, opener_tab_id = ?, title = ?, url = ?, pending_url = ?, pinned = ?, highlighted = ?,
+        window_id = ?, active = ?, favicon_url = ?, incognito = ?, selected = ?, audible = ?, discarded = ?, auto_discardable = ?, muted = ?,
+        muted_reason = ?, muted_extension_id = ?, width = ?, height = ?, session_id = ?, group_id = ?, last_accessed = ?
+        WHERE id = ?
+        "#,
         tab.status,
         tab.tab_index,
         tab.opener_tab_id,
@@ -34,8 +39,11 @@ pub async fn update_tab(State(pool): State<SqlitePool>, Json(tab): Json<BrowserT
         tab.id
     )
     .execute(&pool)
-    .await?
-    .map_err(|e| e.to_string())?;
+    .await
+    .map_err(|e| match e {
+        sqlx::Error::Database(_) => Error::Sqlx(e),
+        _ => Error::Anyhow(e.into()),
+    })?;
 
-	Ok(Json(result))
+	Ok(Json(tab))
 }
