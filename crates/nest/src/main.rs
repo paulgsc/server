a7f7cbfa04464ef847d7954e8389adc73586c69e
Paul Gathondu <aulgondu@gmail.com>
2024-07-27 19:20:19 -0700

    feat: add tracing init

--- crates/nest/src/main.rs
+++ crates/nest/src/main.rs
@@ -1,6 +1,10 @@
 use anyhow::Context;
 use clap::Parser;
 
+use tracing_subscriber::{
+    filter::EnvFilter, fmt::format::JsonFields, util::SubscriberInitExt, Layer
+};
+
 use nest::config::Config;
 use nest::http::serve;
 use sqlx::sqlite::SqlitePoolOptions;
@@ -10,6 +14,8 @@ async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
 	let config = Config::parse();
+    init_tracing(&config);
+
 	// Initialize SQLite connection pool
 	let db = SqlitePoolOptions::new()
 		.max_connections(5)
@@ -24,4 +30,36 @@ async fn main() -> anyhow::Result<()> {
 	serve::serve(config, db).await;
 
 	Ok(())
+}
+
+pub fn init_tracing(config: &Config) -> Option<()> {
+    use std::str::FromStr;
+    use tracing_subscriber::layer::SubscriberExt;
+
+    let filter = EnvFilter::from_str(config.rust_log.as_deref()?).unwrap();
+
+    tracing_subscriber::registry()
+        .with(
+            if config.log_json {
+                Box::new(
+                    tracing_subscriber::fmt::layer()
+                    .fmt_fields(JsonFields::default())
+                    .event_format(
+                        tracing_subscriber::fmt::format()
+                            .json()
+                            .flatten_event(true)
+                            .with_span_list(false),
+                    )
+                    .with_filter(filter),
+                ) as Box<dyn Layer<_> + Send + Sync>
+            } else {
+                Box::new(
+                    tracing_subscriber::fmt::layer()
+                        .event_format(tracing_subscriber::fmt::format().pretty())
+                        .with_filter(filter),
+                )
+            }
+        )
+        .init();
+    None
 }
