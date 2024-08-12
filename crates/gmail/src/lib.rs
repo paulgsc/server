d5a7ca01e656c4f7877dfcea18db67e8b87750c7
Paul Gathondu <aulgondu@gmail.com>
2024-08-12 10:31:37 -0700

    feat: register modules

--- crates/gmail/src/lib.rs
+++ crates/gmail/src/lib.rs
@@ -1,1 +1,11 @@
+pub use hyper;
+pub use hyper_rustils;
+pub extern crate google_apis_common as client;
+pub use client::chrono;
+pub mod api;
 
+pub use api::Gmail;
+pub use client::{Result, Error, Delegate, FieldMask};
+
+#[cfg(feature = "yup-oauth2")]
+pub use client::oauth2;
