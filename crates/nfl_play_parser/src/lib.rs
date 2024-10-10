302dd75f423659c60b846dd88e89ebf737d5b88a
Paul Gathondu <aulgondu@gmail.com>
2024-10-09 20:58:34 -0700

    feat: add play_by_play schema

--- crates/nfl_play_parser/src/lib.rs
+++ crates/nfl_play_parser/src/lib.rs
@@ -1,4 +1,4 @@
 
 pub mod error;
 pub mod schema;
-pub mod parsers;
+
---
+++ crates/nfl_play_parser/src/schema/play_by_play.rs
@@ -0,0 +1,50 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::str::FromStr;
+
+use crate::schema::{DownAndDistance, GameClock, PlayType, ScoringEvent};
+
+
+
+static NEXT_ID: AtomicUsize = AtomicUsize::new(1);
+
+#[derive(Debug, Clone)]
+struct Play {
+    id: usize,
+    game_clock: GameClock,
+    play_type: PlayType,
+    scoring_event: Option<ScoringEvent>,
+    description: String,
+    line: DownAndDistance,
+    yards: i32,
+    players_involved: Vec<Player>,
+}
+
+impl Play {
+    fn next_id() -> usize {
+        NEXT_ID.fetch_add(1, Ordering::SeqCst)
+    }
+
+    // Constructor for a non-scoring play
+    pub fn new(description: String, game_clock: GameClock, play_type: PlayType, line: DownAndDistance) -> Self {
+        Play {
+            id: Play::next_id(),
+            description,
+            game_clock,
+            play_type,
+            line,
+            scoring_event: None,
+        }
+    }
+
+    // Constructor for a scoring play
+    pub fn new_scoring(description: String, game_clock: GameClock, play_type: PlayType, line: DownAndDistance, scoring_event: ScoringEvent) -> Self {
+        Play {
+            id: Play::next_id(),
+            description,
+            game_clock,
+            play_type,
+            line,
+            scoring_event: Some(scoring_event),
+        }
+    }
+}
