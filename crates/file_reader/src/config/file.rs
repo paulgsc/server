ded60a0ab25f46280fcd57a9c1a32fe80a96b752
Paul Gathondu <aulgondu@gmail.com>
2024-10-12 12:00:57 -0700

    feat: add config mod

---
+++ crates/file_reader/src/config/file.rs
@@ -0,0 +1,23 @@
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Parser, Clone, Debug, Serialize, Deserialize)]
+#[command(author, version, about, long_about = None)]
+pub struct FileConfig {
+
+    /// File with HTML soup
+    #[arg(long, env = "HTML_FILE")]
+    pub input_file: String,
+    /// Path to the output CSV file
+    #[arg(long, env = "CSV_OUTPUT", default_value = "data.csv")]
+    pub output_file: String,
+}
+
+
+impl FileConfig {
+    pub fn new() -> Self {
+        Self::parse()
+    }
+}
+
---
+++ crates/file_reader/src/config/mod.rs
@@ -0,0 +1,3 @@
+mod file;
+
+use file::FileConfig;
