c40b20660cdfe88676405335d5f36c2e0a1f99de
Paul Gathondu <aulgondu@gmail.com>
2024-10-12 23:03:36 -0700

    refactor: update server to handle many to many db to routes relationship

--- crates/nest/src/http/handlers/mod.rs
+++ crates/nest/src/http/handlers/mod.rs
@@ -2,3 +2,8 @@ pub mod create_tab;
 pub mod delete_tab;
 pub mod get_tabs;
 pub mod update_tabs;
+
+pub use create_tab::create_tab;
+pub use delete_tab::delete_tab;
+pub use get_tabs::get_all_tabs;
+pub use update_tabs::update_tab;
--- crates/nest/src/http/routes/browser_tabs.rs
+++ crates/nest/src/http/routes/browser_tabs.rs
@@ -1,18 +1,24 @@
-use crate::http::handlers::{create_tab, delete_tab, get_tabs, update_tabs};
+use crate::MultiDbHandler;
+use crate::http::handlers::{create_tab, delete_tab, get_all_tabs, update_tab};
 use axum::routing::{delete, get, post, put};
 use axum::{http::Method, Router};
 use tower_http::cors::{Any, CorsLayer};
 use sqlx::SqlitePool;
 
-pub fn routes(pool: SqlitePool) -> Router {
-	let cors = CorsLayer::new().allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE]).allow_origin(Any);
+pub struct BrowserTabsHandler;
 
-	 Router::new()
-		.route("/api/chrome/tabs/update", put(update_tabs::update_tab))
-		.route("/api/chrome/tabs/delete", delete(delete_tab::delete_tab))
-		.route("/api/chrome/tabs/all", get(get_tabs::get_all_tabs))
-		.route("/api/chrome/tabs/post", post(create_tab::create_tab))
-		.layer(cors)
-        .with_state(pool)
-	
+impl MultiDbHandler for BrowserTabsHandler {
+    fn create_routes(&self, db_name: &str, pool: SqlitePool) -> Router {
+        let cors = CorsLayer::new()
+            .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+            .allow_origin(Any);
+
+        Router::new()
+            .route(&format!("/api/{}/chrome/tabs/update", db_name), put(update_tab))
+            .route(&format!("/api/{}/chrome/tabs/delete", db_name), delete(delete_tab))
+            .route(&format!("/api/{}/chrome/tabs/all", db_name), get(get_all_tabs))
+            .route(&format!("/api/{}/chrome/tabs/post", db_name), post(create_tab))
+            .layer(cors)
+            .with_state(pool)
+    }
 }
