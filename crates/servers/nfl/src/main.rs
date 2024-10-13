ec20f7840dd1d18344d2b3feedc7574d167e3823
Paul Gathondu <aulgondu@gmail.com>
2024-10-13 09:39:46 -0700

    feat: setting up dbs, and servers for nfl

--- crates/servers/nfl/src/main.rs
+++ crates/servers/nfl/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-	println!("Hello, world!");
+pub mod handlers;
+
+use clap::Parser;
+use anyhow::Result;
+use nest::config::Config;
+use nest::{ApiBuilder, MultiDbHandler, Run, init_tracing};
+use nest::http::routes::BrowserTabsHandler;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+	let config = Config::parse();
+    init_tracing(&config);
+
+    let handlers: Vec<Box<dyn MultiDbHandler + Send>> = vec![
+        Box::new(BrowserTabsHandler) as Box<dyn MultiDbHandler + Send>,
+    ];
+
+    ApiBuilder::run(config, handlers).await?;
+
+    Ok(())
+
 }
+
+
