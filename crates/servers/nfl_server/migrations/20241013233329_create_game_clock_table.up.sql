ad6651018667592541e560cffdeb097a779a3d12
Paul Gathondu <aulgondu@gmail.com>
2024-10-13 16:40:42 -0700

    chore: add migrations

---
+++ crates/servers/nfl_server/migrations/20241013233329_create_game_clock_table.up.sql
@@ -0,0 +1,7 @@
+-- Add up migration script here
+CREATE TABLE game_clock (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    minutes INTEGER NOT NULL CHECK (minutes BETWEEN 0 AND 15),
+    seconds INTEGER NOT NULL CHECK (seconds BETWEEN 0 AND 59)
+);
+
