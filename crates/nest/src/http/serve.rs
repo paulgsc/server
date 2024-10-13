b10926ace055037f92e5e144097e2594b0e56641
Paul Gathondu <aulgondu@gmail.com>
2024-10-12 20:51:48 -0700

    refactor: consume mutliple dbs

--- crates/nest/src/http/serve.rs
+++ crates/nest/src/http/serve.rs
@@ -2,6 +2,7 @@ use crate::config::Config;
 use axum::Router;
 use sqlx::SqlitePool;
 use std::sync::Arc;
+use std::collections::HashMap;
 use tokio::net::TcpListener;
 use tower::ServiceBuilder;
 use tower_http::add_extension::AddExtensionLayer;
@@ -12,13 +13,13 @@ use crate::http::routes;
 #[derive(Clone)]
 struct ApiContext {
 	config: Arc<Config>,
-	db: SqlitePool,
+    dbs: HashMap<String, SqlitePool>,
 }
 
-pub async fn serve(config: Config, db: SqlitePool) {
-	let app = api_router(db.clone()).layer(
+pub async fn serve(config: Config, dbs: HashMap<String, SqlitePool>) {
+	let app = api_router(dbs.clone()).layer(
 		ServiceBuilder::new()
-			.layer(AddExtensionLayer::new(ApiContext { config: Arc::new(config), db }))
+			.layer(AddExtensionLayer::new(ApiContext { config: Arc::new(config), dbs }))
 			// Enables logging. Use `RUST_LOG=tower_http=debug`
 			.layer(TraceLayer::new_for_http()),
 	);
@@ -29,7 +30,13 @@ pub async fn serve(config: Config, db: SqlitePool) {
 	axum::serve(listener, app).await.unwrap();
 }
 
-fn api_router(db: SqlitePool) -> Router {
-	// This is the order that the modules were authored in.
-	routes::browser_tabs::routes(db)
+fn api_router(dbs: HashMap<String, SqlitePool>) -> Router {
+    let mut router = Router::new();
+
+    for (db_name, db_pool) in dbs {
+        router = router.merge(routes::browser_tabs::routes(db_name, db_pool));
+    }
+
+    router
 }
+
