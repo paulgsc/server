ca5363a64b2447e9e47979075e663ba3c0eedd72
Paul Gathondu <aulgondu@gmail.com>
2024-10-13 23:23:00 -0700

    chore: run fmt

--- crates/servers/nfl_server/src/commands/mod.rs
+++ crates/servers/nfl_server/src/commands/mod.rs
@@ -1,23 +1,23 @@
-use anyhow::Result;
-use sqlx::SqlitePool;
-use nest::config::Config;
-use crate::models::game_clock::{CreateGameClock, GameClock};
 use crate::common::CrudOperations;
+use crate::models::game_clock::{CreateGameClock, GameClock};
+use anyhow::Result;
+use nest::config::Config;
+use sqlx::SqlitePool;
 
 pub async fn populate_game_clocks(config: &Config) -> Result<()> {
-    let pool = SqlitePool::connect(&config.database_urls).await?;
+	let pool = SqlitePool::connect(&config.database_urls).await?;
 
-    let mut game_clocks = Vec::new();
-    for minutes in 0..=15 {
-        for seconds in 0..=59 {
-            game_clocks.push(CreateGameClock { minutes, seconds });
-        }
-    }
+	let mut game_clocks = Vec::new();
+	for minutes in 0..=15 {
+		for seconds in 0..=59 {
+			game_clocks.push(CreateGameClock { minutes, seconds });
+		}
+	}
 
-    match GameClock::batch_create(&pool, &game_clocks).await {
-        Ok(created) => println!("Successfully inserted {} GameClock records", created.len()),
-        Err(err) => eprintln!("Failed to insert GameClock records: {:?}", err),
-    }
+	match GameClock::batch_create(&pool, &game_clocks).await {
+		Ok(created) => println!("Successfully inserted {} GameClock records", created.len()),
+		Err(err) => eprintln!("Failed to insert GameClock records: {:?}", err),
+	}
 
-    Ok(())
+	Ok(())
 }
