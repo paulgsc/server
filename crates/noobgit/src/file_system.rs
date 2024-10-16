2a9933a9372d4761e3e856af0f547d3ca5ed92e2
Paul Gathondu <aulgondu@gmail.com>
2024-10-16 16:27:34 -0700

    refactor: update debouncer logic

--- crates/noobgit/src/file_system.rs
+++ crates/noobgit/src/file_system.rs
@@ -1,5 +1,5 @@
 use std::path::{Path, PathBuf};
-use std::fs;
+use tokio::fs;
 
 pub struct FileSystem {
     root: PathBuf,
@@ -9,31 +9,30 @@ impl FileSystem {
     pub fn new<P: AsRef<Path>>(root: P) -> Result<Self, std::io::Error> {
         let root = root.as_ref().to_path_buf();
         if !root.exists() {
-            fs::create_dir_all(&root)?;
+            std::fs::create_dir_all(&root)?;
         }
         Ok(Self { root })
     }
 
-    pub fn add<P: AsRef<Path>>(&self, path: P) -> Result<(), std::io::Error> {
+    pub async fn add<P: AsRef<Path>>(&self, path: P) -> Result<(), std::io::Error> {
         let path = self.root.join(path);
         if path.is_file() {
-            // File already exists, no need to create
             Ok(())
         } else if path.is_dir() {
-            fs::create_dir_all(path)
+            fs::create_dir_all(path).await
         } else {
-            fs::File::create(path).map(|_| ())
+            fs::File::create(path).await.map(|_| ())
         }
     }
 
-    pub fn remove<P: AsRef<Path>>(&self, path: P) -> Result<(), std::io::Error> {
+    pub async fn remove<P: AsRef<Path>>(&self, path: P) -> Result<(), std::io::Error> {
         let path = self.root.join(path);
         if path.is_file() {
-            fs::remove_file(path)
+            fs::remove_file(path).await
         } else if path.is_dir() {
-            fs::remove_dir_all(path)
+            fs::remove_dir_all(path).await
         } else {
-            Ok(()) // Path doesn't exist, no need to remove
+            Ok(())
         }
     }
 }
