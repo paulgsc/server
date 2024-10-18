6fd01f9d7fef6604eeb758129c9f002061738f60
Paul Gathondu <aulgondu@gmail.com>
2024-10-18 13:31:10 -0700

    test: add tests

--- crates/noobgit/src/registry.rs
+++ crates/noobgit/src/registry.rs
@@ -63,3 +63,67 @@ impl Registry {
 			.collect()
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_new_registry() {
+		let registry = Registry::new();
+		assert_eq!(registry.unstaged_changes.len(), 0);
+		assert_eq!(registry.staged_changes.len(), 0);
+		assert_eq!(registry.max_changes, 5);
+	}
+
+	#[test]
+	fn test_add_change() {
+		let mut registry = Registry::new();
+		let change = Change::new(ChangeType::Create, PathBuf::from("/test/file.txt"));
+		registry.add_change(change);
+		assert_eq!(registry.unstaged_changes.len(), 1);
+	}
+
+	#[test]
+	fn test_add_change_max_limit() {
+		let mut registry = Registry::new();
+		for i in 0..6 {
+			let change = Change::new(ChangeType::Create, PathBuf::from(format!("/test/file{}.txt", i)));
+			registry.add_change(change);
+		}
+		assert_eq!(registry.unstaged_changes.len(), 5);
+		assert_eq!(registry.unstaged_changes.front().unwrap().path, PathBuf::from("/test/file1.txt"));
+	}
+
+	#[test]
+	fn test_stage_changes() {
+		let mut registry = Registry::new();
+		registry.add_change(Change::new(ChangeType::Create, PathBuf::from("/test/file1.txt")));
+		registry.add_change(Change::new(ChangeType::Delete, PathBuf::from("/test/file2.txt")));
+		registry.stage_changes();
+		assert_eq!(registry.unstaged_changes.len(), 0);
+		assert_eq!(registry.staged_changes.len(), 2);
+	}
+
+	#[test]
+	fn test_unstage_changes() {
+		let mut registry = Registry::new();
+		registry.add_change(Change::new(ChangeType::Create, PathBuf::from("/test/file1.txt")));
+		registry.add_change(Change::new(ChangeType::Delete, PathBuf::from("/test/file2.txt")));
+		registry.stage_changes();
+		registry.unstage_changes();
+		assert_eq!(registry.unstaged_changes.len(), 2);
+		assert_eq!(registry.staged_changes.len(), 0);
+	}
+
+	#[test]
+	fn test_get_notifications() {
+		let mut registry = Registry::new();
+		registry.add_change(Change::new(ChangeType::Create, PathBuf::from("/test/file1.txt")));
+		registry.add_change(Change::new(ChangeType::Delete, PathBuf::from("/test/file2.txt")));
+		let notifications = registry.get_notifications();
+		assert_eq!(notifications.len(), 2);
+		assert_eq!(notifications[0], "Created /test/file1.txt");
+		assert_eq!(notifications[1], "Deleted /test/file2.txt");
+	}
+}
