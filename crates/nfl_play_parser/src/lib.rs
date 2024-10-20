3b81d2c7325c85cd6ab980b0cc934c7dc4c3b781
Paul Gathondu <aulgondu@gmail.com>
2024-10-19 20:40:25 -0700

    feat: setup playparser hmtl file reader

--- crates/nfl_play_parser/src/lib.rs
+++ crates/nfl_play_parser/src/lib.rs
@@ -1,3 +1,24 @@
 pub mod error;
 pub mod query_selectors;
 pub mod schema;
+
+use scraper::Html;
+use async_trait::async_trait;
+use file_reader::HtmlProcessor;
+use crate::query_selectors::{parse_play_descriptions, ParsedSelectors};
+
+pub struct PlayHtmlProcessor;
+
+#[async_trait]
+impl HtmlProcessor for PlayHtmlProcessor {
+    async fn process_html_content(&self, html_content: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let document = Html::parse_document(html_content);
+        let parsed_selectors = ParsedSelectors::new();
+        for description in parse_play_descriptions(&document, &parsed_selectors) {
+            println!("{}", description);
+        }
+        Ok(())
+    }
+}
+
+
