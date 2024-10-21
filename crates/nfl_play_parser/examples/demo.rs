68606afee9146c5b9362fd0d9658c130335f8d32
Paul Gathondu <aulgondu@gmail.com>
2024-10-21 00:04:37 -0700

    chore: fmt fix, deps updates

--- crates/nfl_play_parser/examples/demo.rs
+++ crates/nfl_play_parser/examples/demo.rs
@@ -1,6 +1,6 @@
 use nfl_play_parser::query_selectors::{parse_play_descriptions, ParsedSelectors};
-use nfl_play_parser::schema::Play;
 use nfl_play_parser::read_html_file;
+use nfl_play_parser::schema::Play;
 
 fn main() {
 	let file_path = "examples/demo.html";
@@ -10,10 +10,10 @@ fn main() {
 			let selectors = ParsedSelectors::new();
 
 			for description in parse_play_descriptions(&document, &selectors) {
-                match Play::try_from(description) {
-                    Ok(play) => println!("{:?}", play),
-                    Err(err) => eprintln!("Error parsing play: {}", err),
-                }
+				match Play::try_from(description) {
+					Ok(play) => println!("{:?}", play),
+					Err(err) => eprintln!("Error parsing play: {}", err),
+				}
 			}
 		}
 		Err(e) => {
