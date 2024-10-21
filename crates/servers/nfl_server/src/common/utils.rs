9166b26060780c186cf9a52230098fe9f49b4c22
Paul Gathondu <aulgondu@gmail.com>
2024-10-21 16:35:46 -0700

    fix: lifetimes fix, pass ownership

--- crates/servers/nfl_server/src/common/utils.rs
+++ crates/servers/nfl_server/src/common/utils.rs
@@ -5,9 +5,9 @@ use nest::http::Error;
 
 #[async_trait]
 pub trait CrudOperations<T, C> {
-	async fn create(pool: &SqlitePool, item: &C) -> Result<T, Error>;
+	async fn create(pool: &SqlitePool, item: C) -> Result<T, Error>;
 	async fn batch_create(pool: &SqlitePool, items: &[C]) -> Result<Vec<T>, Error>;
 	async fn get(pool: &SqlitePool, id: i64) -> Result<T, Error>;
-	async fn update(pool: &SqlitePool, id: i64, item: &C) -> Result<T, Error>;
+	async fn update(pool: &SqlitePool, id: i64, item: C) -> Result<T, Error>;
 	async fn delete(pool: &SqlitePool, id: i64) -> Result<(), Error>;
 }
