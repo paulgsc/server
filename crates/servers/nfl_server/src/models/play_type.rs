6764169c7b6c33bf2fda0f0906133a37cb125b13
Paul Gathondu <aulgondu@gmail.com>
2024-10-21 19:54:06 -0700

    chore: refactor to use nfl server error eunm

--- crates/servers/nfl_server/src/models/play_type.rs
+++ crates/servers/nfl_server/src/models/play_type.rs
@@ -21,14 +21,14 @@ pub struct CreatePlayType {
 #[derive(sqlx::FromRow)]
 struct PlayTypeRecordRaw {
 	id: i64,
-	name: String,
+	play_type: String,
 }
 
 #[async_trait]
 impl CrudOperations<PlayTypeRecord, CreatePlayType> for PlayTypeRecord {
 	async fn create(pool: &SqlitePool, item: CreatePlayType) -> Result<PlayTypeRecord, Error> {
 		let play_type_str = item.play_type.to_string();
-		let result = sqlx::query!("INSERT INTO play_types (name) VALUES (?)", play_type_str).execute(pool).await?;
+		let result = sqlx::query!("INSERT INTO play_types (play_type) VALUES (?)", play_type_str).execute(pool).await .map_err(NestError::from)?;
 
 		Ok(PlayTypeRecord {
 			id: result.last_insert_rowid(),
@@ -42,9 +42,9 @@ impl CrudOperations<PlayTypeRecord, CreatePlayType> for PlayTypeRecord {
 
 		for item in items {
 			let play_type_str = item.play_type.to_string();
-			let result = sqlx::query!("INSERT INTO play_types (name) VALUES (?)", play_type_str)
+			let result = sqlx::query!("INSERT INTO play_types (play_type) VALUES (?)", play_type_str)
 				.execute(&mut *tx)
-                .await?;
+                .await.map_err(NestError::from)?;
 
 			created_records.push(PlayTypeRecord {
 				id: result.last_insert_rowid(),
@@ -58,20 +58,20 @@ impl CrudOperations<PlayTypeRecord, CreatePlayType> for PlayTypeRecord {
 	}
 
 	async fn get(pool: &SqlitePool, id: i64) -> Result<PlayTypeRecord, Error> {
-		let record = sqlx::query_as!(PlayTypeRecordRaw, "SELECT id, name FROM play_types WHERE id = ?", id)
+		let record = sqlx::query_as!(PlayTypeRecordRaw, "SELECT id, play_type FROM play_types WHERE id = ?", id)
 			.fetch_optional(pool)
-			.await?
+			.await.map_err(NestError::from)?
 			.ok_or(Error::NestError(NestError::NotFound))?;
 
 		Ok(PlayTypeRecord {
 			id: record.id,
-			play_type: PlayType::from_str(&record.name)?,
+			play_type: PlayType::from_str(&record.play_type)?,
 		})
 	}
 
 	async fn update(pool: &SqlitePool, id: i64, item: CreatePlayType) -> Result<PlayTypeRecord, Error> {
 		let play_type_str = item.play_type.to_string();
-		let result = sqlx::query!("UPDATE play_types SET name = ? WHERE id = ?", play_type_str, id).execute(pool).await?;
+		let result = sqlx::query!("UPDATE play_types SET play_type = ? WHERE id = ?", play_type_str, id).execute(pool).await.map_err(NestError::from)?;
 
 		if result.rows_affected() == 0 {
 			return Err(Error::NestError(NestError::NotFound));
@@ -81,7 +81,7 @@ impl CrudOperations<PlayTypeRecord, CreatePlayType> for PlayTypeRecord {
 	}
 
 	async fn delete(pool: &SqlitePool, id: i64) -> Result<(), Error> {
-		let result = sqlx::query!("DELETE FROM play_types WHERE id = ?", id).execute(pool).await?;
+		let result = sqlx::query!("DELETE FROM play_types WHERE id = ?", id).execute(pool).await.map_err(NestError::from)?;
 
 		if result.rows_affected() == 0 {
 			return Err(Error::NestError(NestError::NotFound));
