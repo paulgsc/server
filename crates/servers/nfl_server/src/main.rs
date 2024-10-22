1dfe4106de89c63705ae13dafba50d9898fe2424
Paul Gathondu <aulgondu@gmail.com>
2024-10-21 21:16:35 -0700

    register playtypes routes

--- crates/servers/nfl_server/src/main.rs
+++ crates/servers/nfl_server/src/main.rs
@@ -5,7 +5,7 @@ pub mod models;
 pub mod routes;
 
 use crate::commands::populate_game_clocks;
-use crate::handlers::{GameClockHandlers, GameClockMigrationHandler};
+use crate::handlers::{GameClockHandlers, GameClockMigrationHandler ,PlayTypeHandlers, PlayTypeMigrationHandler};
 use anyhow::Result;
 use clap::Parser;
 use nest::config::{Config, ProgramMode};
@@ -22,7 +22,10 @@ async fn main() -> Result<()> {
 			populate_game_clocks(&config).await?;
 		}
 		ProgramMode::Run => {
-			let handlers: Vec<Box<dyn MultiDbHandler + Send>> = vec![Box::new(GameClockHandlers) as Box<dyn MultiDbHandler + Send>];
+            let handlers: Vec<Box<dyn MultiDbHandler + Send>> = vec![
+                Box::new(GameClockHandlers) as Box<dyn MultiDbHandler + Send>,
+                Box::new(PlayTypeHandlers) as Box<dyn MultiDbHandler + Send>,
+                  ];
 			ApiBuilder::run(config, handlers, GameClockMigrationHandler).await?;
 		}
 	}
