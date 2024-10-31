944004929371ce9a4252e5b381455153172434d7
Paul Gathondu <aulgondu@gmail.com>
2024-10-31 16:25:15 -0700

    refactor: remove unwrap

--- crates/task_queue/src/error.rs
+++ crates/task_queue/src/error.rs
@@ -1,3 +1,4 @@
+use std::num::TryFromIntError;
 use std::time::SystemTimeError;
 use thiserror::Error;
 
@@ -17,10 +18,20 @@ pub enum KnownError {
 	RedisError(#[from] redis::RedisError),
 	#[error("Prometheus error: {0}")]
 	PrometheusError(#[from] prometheus::Error),
+	#[error("JSON error: {0}")]
+	JsonError(#[from] serde_json::Error),
+	#[error("Conversion error: {0}")]
+	ConversionError(String),
 }
 
 impl From<SystemTimeError> for KnownError {
 	fn from(error: SystemTimeError) -> Self {
 		Self::InternalError(format!("System time error: {error}"))
+	}
+}
+
+impl From<TryFromIntError> for KnownError {
+	fn from(error: TryFromIntError) -> Self {
+		Self::ConversionError(format!("Conversion error: {error}"))
 	}
 }
