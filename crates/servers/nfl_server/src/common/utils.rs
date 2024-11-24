15d5b3868673a41c167210e32ba7050b3c3138e4
Paul Gathondu <aulgondu@gmail.com>
2024-11-23 20:18:15 -0800

    refactor: move model id, identifiable trait to utils

--- crates/servers/nfl_server/src/common/utils.rs
+++ crates/servers/nfl_server/src/common/utils.rs
@@ -1,9 +1,38 @@
 use crate::common::nfl_server_error::NflServerError as Error;
 use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ModelId<T>(u32, std::marker::PhantomData<T>);
+
+impl<T> ModelId<T> {
+	pub const fn new(id: u32) -> Self {
+		Self(id, std::marker::PhantomData)
+	}
+
+	pub const fn value(&self) -> u32 {
+		self.0
+	}
+}
+
+pub trait Identifiable {
+	fn model_id(&self) -> ModelId<Self>
+	where
+		Self: Sized,
+	{
+		ModelId::new(self.id())
+	}
+
+	fn id(&self) -> u32;
+}
+
 #[async_trait]
-pub trait CrudOperations<T, C> {
+pub trait CrudOperations<T, C>
+where
+	T: Send + Sync + 'static,
+	C: Send + Sync + 'static,
+{
 	async fn create(pool: &SqlitePool, item: C) -> Result<T, Error>;
 	async fn batch_create(pool: &SqlitePool, items: &[C]) -> Result<Vec<T>, Error>;
 	async fn get(pool: &SqlitePool, id: i64) -> Result<T, Error>;
