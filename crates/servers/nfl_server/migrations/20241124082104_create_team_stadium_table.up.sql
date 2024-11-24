f999b1ad4d54977c07b1f31552f41b4ff587ac8f
Paul Gathondu <aulgondu@gmail.com>
2024-11-24 02:25:22 -0800

    chore: create team_stadiums table migrations

---
+++ crates/servers/nfl_server/migrations/20241124082104_create_team_stadium_table.up.sql
@@ -0,0 +1,11 @@
+-- Add up migration script here
+CREATE TABLE stadiums ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    state INTEGER NOT NULL,
+    city TEXT NOT NULL,
+    stadium_type INTEGER NOT NULL,
+    surface_type INTEGER NOT NULL,
+    capacity INTEGER NOT NULL,
+    CONSTRAINT name_unique UNIQUE (name)
+);
