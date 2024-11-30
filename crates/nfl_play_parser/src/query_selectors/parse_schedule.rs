fa96a4e430ab428020576d1b5cbaa88aa664fb45
Paul Gathondu <aulgondu@gmail.com>
2024-11-29 20:55:39 -0800

    feat: add year token

--- crates/nfl_play_parser/src/query_selectors/parse_schedule.rs
+++ crates/nfl_play_parser/src/query_selectors/parse_schedule.rs
@@ -1,8 +1,16 @@
 use scraper::{Html, Selector};
 use std::fmt;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ScheduleParserError {
+	#[error("Missing NFL Year token")]
+	MissingNFLYear,
+}
 
 pub struct NflGameScheduleSelectors {
 	pub schedule_section: Selector,
+	pub nfl_year: Selector,
 	pub date_header: Selector,
 	pub matchup_strip: Selector,
 	pub schedule_desc: Selector,
@@ -13,6 +21,7 @@ impl NflGameScheduleSelectors {
 	pub fn new() -> Self {
 		NflGameScheduleSelectors {
 			schedule_section: Selector::parse(".nfl-o-matchup-group").unwrap(),
+			nfl_year: Selector::parse(".nfl-o-page-title").unwrap(),
 			date_header: Selector::parse(".d3-o-section-title").unwrap(),
 			matchup_strip: Selector::parse(".nfl-c-matchup-strip").unwrap(),
 			schedule_desc: Selector::parse("a.nfl-c-matchup-strip__left-area").unwrap(),
@@ -23,6 +32,7 @@ impl NflGameScheduleSelectors {
 
 #[derive(Debug)]
 pub struct GameInfo {
+	pub nfl_year: String,
 	pub date: String,
 	pub schedule_desc: String,
 	pub status: String,
@@ -30,25 +40,37 @@ pub struct GameInfo {
 
 impl fmt::Display for GameInfo {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "Date: {}\nStatus: {}\nSchedule_desc: {}", self.date, self.status, self.schedule_desc,)
+		write!(
+			f,
+			"NFL Year: {} Date: {}\nStatus: {}\nSchedule_desc: {}",
+			self.nfl_year, self.date, self.status, self.schedule_desc,
+		)
 	}
 }
 
 pub struct NflGameScheduleIterator<'a> {
 	schedule_iter: scraper::html::Select<'a, 'a>,
+	nfl_year: String,
 	current_date: Option<String>,
 	matchup_iter: Option<scraper::element_ref::Select<'a, 'a>>,
 	selectors: &'a NflGameScheduleSelectors,
 }
 
 impl<'a> NflGameScheduleIterator<'a> {
-	pub fn new(document: &'a Html, selectors: &'a NflGameScheduleSelectors) -> Self {
-		NflGameScheduleIterator {
+	pub fn new(document: &'a Html, selectors: &'a NflGameScheduleSelectors) -> Result<Self, ScheduleParserError> {
+		let nfl_year = document
+			.select(&selectors.nfl_year)
+			.next()
+			.ok_or_else(|| ScheduleParserError::MissingNFLYear)
+			.map(|el| el.inner_html())?;
+
+		Ok(NflGameScheduleIterator {
 			schedule_iter: document.select(&selectors.schedule_section),
+			nfl_year,
 			current_date: None,
 			matchup_iter: None,
 			selectors,
-		}
+		})
 	}
 }
 
@@ -68,7 +90,8 @@ impl<'a> Iterator for NflGameScheduleIterator<'a> {
 					let status = matchup.select(&self.selectors.game_status).next().map(|el| el.inner_html()).unwrap_or_default();
 
 					return Some(GameInfo {
-						date: self.current_date.clone().unwrap_or_default(),
+						nfl_year: self.nfl_year.clone(),
+						date: self.current_date.clone().unwrap(),
 						schedule_desc,
 						status,
 					});
@@ -87,8 +110,8 @@ impl<'a> Iterator for NflGameScheduleIterator<'a> {
 	}
 }
 
-pub fn parse_nfl_game_schedule<'a>(document: &'a Html, selectors: &'a NflGameScheduleSelectors) -> NflGameScheduleIterator<'a> {
-	NflGameScheduleIterator::new(document, selectors)
+pub fn parse_nfl_game_schedule<'a>(document: &'a Html, selectors: &'a NflGameScheduleSelectors) -> Result<NflGameScheduleIterator<'a>, ScheduleParserError> {
+	NflGameScheduleIterator::new(document, selectors).map_err(|_| ScheduleParserError::MissingNFLYear)
 }
 
 // Example usage
