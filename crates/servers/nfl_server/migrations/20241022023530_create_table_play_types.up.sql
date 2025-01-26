3d6c46059d95569ee52b6c925420132ead5aa012
Paul Gathondu <aulgondu@gmail.com>
2025-01-26 00:16:51 -0800

    refactor: use if exists

--- crates/servers/nfl_server/migrations/20241022023530_create_table_play_types.up.sql
+++ crates/servers/nfl_server/migrations/20241022023530_create_table_play_types.up.sql
@@ -1,7 +1,6 @@
 -- Add up migration script here
 -- Up migration to create the play_types table
-CREATE TABLE play_types (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    play_type TEXT NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS play_types (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  play_type TEXT NOT NULL UNIQUE
 );
-
