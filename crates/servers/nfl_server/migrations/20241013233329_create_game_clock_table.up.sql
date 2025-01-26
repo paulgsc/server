03cfb37ecee09f401c09de90d8ac364c749fb986
Paul Gathondu <aulgondu@gmail.com>
2025-01-26 00:16:51 -0800

    refactor: use if exists

--- crates/servers/nfl_server/migrations/20241013233329_create_game_clock_table.up.sql
+++ crates/servers/nfl_server/migrations/20241013233329_create_game_clock_table.up.sql
@@ -1,7 +1,6 @@
 -- Add up migration script here
-CREATE TABLE game_clock (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    minutes INTEGER NOT NULL CHECK (minutes BETWEEN 0 AND 15),
-    seconds INTEGER NOT NULL CHECK (seconds BETWEEN 0 AND 59)
+CREATE TABLE IF NOT EXISTS game_clock (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  minutes INTEGER NOT NULL CHECK (minutes BETWEEN 0 AND 15),
+  seconds INTEGER NOT NULL CHECK (seconds BETWEEN 0 AND 59)
 );
-
