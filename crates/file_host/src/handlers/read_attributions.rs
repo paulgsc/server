130672d224599f162fb342d77aa4325c56ccd474
Paul Gathondu <aulgondu@gmail.com>
2025-02-01 01:34:27 -0800

    refactor: use crate defined error

--- crates/file_host/src/handlers/read_attributions.rs
+++ crates/file_host/src/handlers/read_attributions.rs
@@ -1,7 +1,8 @@
+use crate::FileHostError;
 use axum::extract::Path;
-use nest::http::Error;
+use axum::Json;
 
-pub async fn get(Path(id): Path<i64>) -> Result<(), Error> {
+pub async fn get(Path(id): Path<i64>) -> Result<Json<&'static str>, FileHostError> {
 	println!("this is the path id: {id}");
-	Ok(())
+	Ok(Json("hello world"))
 }
