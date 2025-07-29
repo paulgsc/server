fe4cf45ba4ecd1d1252f80dffad406b79193b456
Paul Gathondu <aulgondu@gmail.com>
2025-07-28 18:01:19 -0700

    fix: rip

--- crates/servers/chrome_tabs_server/src/handlers/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod create_tab;
-pub mod delete_tab;
-pub mod get_tabs;
-pub mod update_tabs;
-
-pub use create_tab::create_tab;
-pub use delete_tab::delete_tab;
-pub use get_tabs::get_all_tabs;
-pub use update_tabs::update_tab;
